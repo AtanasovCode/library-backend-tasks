ad0c3b89206cb4afc16e04729c4e94af448b2718
AtanasovCode <a_ljupce@yahoo.com>
2026-06-02 17:18:09 +0200

    complete tasks for book repository

--- repository/book_repository.py
+++ repository/book_repository.py
@@ -14,14 +14,35 @@ def find_by_id(db: Session, book_id: int):
 
 def save(db: Session, book_create: BookCreate):
     # TODO 8: Create a new Book instance and save it to the database
-    pass
+    new_book = Book(**book_create.model_dump())
+
+    db.add(new_book)
+    db.commit()
+    db.refresh(new_book)
+    return new_book
 
 
 def update(db: Session, book_update: BookUpdate, book_id: int):
     # TODO 9: Find the book, and if it exists, update only the fields provided in book_update
-    pass
+    db_book = find_by_id(db, book_id)
+
+    if not db_book:
+        return None
+
+    for key, value in book_update.model_dump(exclude_unset=True).items():
+        setattr(db_book, key, value)
+
+    db.commit()
+    db.refresh(db_book)
+    return db_book
 
 
 def delete_by_id(db: Session, book_id: int):
     # TODO 10: Find the book and remove it from the database
-    pass
+    db_book = find_by_id(db, book_id)
+
+    if not db_book:
+        return None
+    db.delete(db_book)
+    db.commit()
+    return db_book
